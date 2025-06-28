38add6d0166d59c877c11ffc8316426028caffb0
Markus Dullnig <markusdullnig@hotmail.com>
2025-06-28 16:32:47 +0200

    update archer enemy

--- Assets/Scripts/Units/Enemy.cs
+++ Assets/Scripts/Units/Enemy.cs
@@ -7,21 +7,16 @@ using UnityEngine.Splines;
 public class Enemy : Unit
 {
     private SplineContainer _movePath;
-    private SplineAnimate _animate;
+    protected SplineAnimate _animate;
 
-    [SerializeField] private float _moveSpeed = 3f;
-    [SerializeField] private float _speedWhileAttacking;
+    [SerializeField] protected float _moveSpeed = 3f;
+
 
     [SerializeField] private int _spawnCost;
 
     [SerializeField] private int _damageToThrone;
-    [SerializeField] private int _attackDamage;
-    [SerializeField] private Arrow _arrow;
+    [SerializeField] protected int _attackDamage;
 
-    [SerializeField] private float _fireRate;
-    private float _fireTime;
-    private float _passedTimeShooting;
-    private Transform _attackTarget;
 
     private float originalSpeed;
 
@@ -35,21 +30,9 @@ public class Enemy : Unit
         }
     }
 
-    public float FireRate {
-        get {
-            return _fireRate;
-        }
 
-        set {
-            _fireRate = value;
-            UpdateFireTime();
-        }
-    }
-    private void UpdateFireTime() {
-        _fireTime = 1f / _fireRate;
-    }
 
-    private void Start() {
+    protected virtual void Start() {
         _movePath = GameInstance.Instance.MovePaths.GetRandomMovePath();
         _animate = GetComponent<SplineAnimate>();
         _animate.Container = _movePath;
@@ -63,39 +46,11 @@ public class Enemy : Unit
         _animate.StartOffset = distanceOnSpline;
 
         _animate.Play();
-        UpdateFireTime();
+
     }
 
-    private void Update() {
-        ShootArrow();
-        if(_attackTarget != null) {
-            if(_speedWhileAttacking == 0f) {
-                _animate.Pause();
-            }
-            float normalizedTime = _animate.NormalizedTime;
-            _animate.MaxSpeed = _speedWhileAttacking;
-            _animate.NormalizedTime = normalizedTime;
-        }
-        else {
 
-            float normalizedTime = _animate.NormalizedTime;
-            _animate.MaxSpeed = _moveSpeed;
-            _animate.NormalizedTime = normalizedTime;
-            if(_animate.IsPlaying == false) {
-                _animate.Play();
-            }
-        }
-    }
 
-    private void ShootArrow() {
-        _passedTimeShooting = _passedTimeShooting + Time.deltaTime;
-        if(_fireTime > _passedTimeShooting) {
-            return;
-        }
-        _passedTimeShooting = _passedTimeShooting - _fireTime;
-
-        SpawnArrow(_attackTarget);
-    }
 
 
     public bool IsDead() {
@@ -163,42 +118,5 @@ public class Enemy : Unit
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
-        if(_arrow == null) {
-            return;
-        }
-        _attackTarget = collision.gameObject.transform;
-    }
-
-    private void OnTriggerStay2D(Collider2D collision) {
-        if(_arrow == null) {
-            return;
-        }
-        if(_attackTarget == null) {
-            _attackTarget = collision.gameObject.transform;
-        }
-        if(_attackTarget.TryGetComponent(out Tower tower)) {
-            if(tower.IsAlive()) {
-                return;
-            }
-            else {
-                _attackTarget = collision.gameObject.transform;
-            }
-        }
-    }
-
-    private void OnTriggerExit2D(Collider2D collision) {
-        _attackTarget = null;
-    }
-
-    private void SpawnArrow(Transform target) {
-        if(target == null) {
-            return;
-        }
-
-        Arrow arrow = Instantiate(_arrow,transform.position,Quaternion.identity);
-        arrow.SetTarget(target);
-        arrow.Damage = _attackDamage;
-    }
 
 }
