c3786673acba5f123522a9247af06685eacfe796
Markus Dullnig <markusdullnig@hotmail.com>
2025-06-28 17:45:47 +0200

    hide unhurt healthbar

--- Assets/Scripts/Units/HealthBarTower.cs
+++ Assets/Scripts/Units/HealthBarTower.cs
@@ -5,12 +5,21 @@ public class HealthBarTower : MonoBehaviour
 {
     [SerializeField] private Image _barImage;
     [SerializeField] private Tower _unit;
+    [SerializeField] private Image _barBackground;
 
     // Update is called once per frame
     void Update() {
         IHealth healthValues = _unit;
 
         float procent = healthValues.Health / healthValues.MaxHealth;
+        if(procent >= 1f) {
+            _barImage.enabled = false;
+            _barBackground.enabled = false;
+        }
+        else {
+            _barImage.enabled = true;
+            _barBackground.enabled = true;
+        }
         _barImage.fillAmount = procent;
     }
 }
--- Assets/Scripts/Units/HealthBarUnit.cs
+++ Assets/Scripts/Units/HealthBarUnit.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class HealthBarUnit : MonoBehaviour
 {
     [SerializeField] private Image _barImage;
+    [SerializeField] private Image _barBackground;
     [SerializeField] private Unit _unit;
 
     // Update is called once per frame
@@ -11,6 +12,14 @@ public class HealthBarUnit : MonoBehaviour
         IHealth healthValues = _unit;
 
         float procent = healthValues.Health / healthValues.MaxHealth;
+        if(procent >= 1f) {
+            _barImage.enabled = false;
+            _barBackground.enabled = false;
+        }
+        else {
+            _barImage.enabled = true;
+            _barBackground.enabled = true;
+        }
         _barImage.fillAmount = procent;
     }
 }
