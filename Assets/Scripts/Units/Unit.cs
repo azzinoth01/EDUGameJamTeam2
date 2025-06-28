27e7c3834889270bf2386ab8f34828999b271b7e
Markus Dullnig <markusdullnig@hotmail.com>
2025-06-28 12:20:39 +0200

    gold on death

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -4,13 +4,19 @@ public class Unit : MonoBehaviour
 {
     [SerializeField] protected float _health;
     [SerializeField] protected int _attackPower;
+    [SerializeField] protected int _goldOnDeath;
 
 
     public void TakeDmg(float damage) {
 
         _health = _health - damage;
         if(_health <= 0) {
-            Destroy(gameObject);
+            Death();
         }
+    }
+
+    private void Death() {
+        GameInstance.Instance.Player.GetGold(_goldOnDeath);
+        Destroy(gameObject);
     }
 }
