458b1c7dc8200198adb23d6b977206030d6c1c12
Markus Dullnig <markusdullnig@hotmail.com>
2025-06-29 20:24:42 +0200

    web build

--- Assets/Scripts/Player/PauseManager.cs
+++ Assets/Scripts/Player/PauseManager.cs
@@ -8,44 +8,37 @@ public class PauseManager : MonoBehaviour
 
     private bool isPaused = false;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape))
-        {
-            if (isPaused)
+    void Update() {
+        if(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape)) {
+            if(isPaused)
                 ResumeGame();
             else
                 PauseGame();
         }
     }
 
-    public void PauseGame()
-    {
+    public void PauseGame() {
         pauseCanvas.SetActive(true);
         Time.timeScale = 0f;
         isPaused = true;
 
-        foreach (var ui in gameplayCanvasesToDisable)
-        {
+        foreach(var ui in gameplayCanvasesToDisable) {
             ui.SetActive(false);
         }
     }
 
-    public void ResumeGame()
-    {
+    public void ResumeGame() {
         pauseCanvas.SetActive(false);
         Time.timeScale = 1f;
         isPaused = false;
 
-        foreach (var ui in gameplayCanvasesToDisable)
-        {
-            ui.SetActive(true); 
+        foreach(var ui in gameplayCanvasesToDisable) {
+            ui.SetActive(true);
         }
     }
 
-    public void ReturnToMainMenu()
-    {
+    public void ReturnToMainMenu() {
         Time.timeScale = 1f;
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(0);
     }
 }
